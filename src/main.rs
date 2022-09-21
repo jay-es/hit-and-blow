16361f04b091e9c2dcc9170147051c81237de8fc
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-09-21 23:36:19 +0900

    ユーザー入力作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod lib;
+
+use crate::lib::{CompareResult, Digits};
+
+/** ユーザー入力 */
+fn input() -> Digits {
+    loop {
+        println!("input digits");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).ok();
+        let str = input.trim();
+
+        match Digits::new_from_str(str) {
+            Ok(v) => break v,
+            Err(e) => println!("{}", e),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_digits: Digits = input();
 }
