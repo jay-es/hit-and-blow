a381f30243d1cd2324e6f64808d7475fd8b6a706
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-09-22 00:05:26 +0900

    メイン処理作成

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn input() -> Digits {
     loop {
         println!("input digits");
         let mut input = String::new();
-        std::io::stdin().read_line(&mut input).ok();
+        std::io::stdin().read_line(&mut input).unwrap();
         let str = input.trim();
 
         match Digits::new_from_str(str) {
@@ -18,5 +18,22 @@ fn input() -> Digits {
 }
 
 fn main() {
-    let my_digits: Digits = input();
+    let ans = Digits::generate_random();
+    let mut turn: u32 = 0;
+
+    loop {
+        turn += 1;
+        println!("turn: {}", turn);
+        let my_digits: Digits = input();
+
+        match my_digits.compare(&ans) {
+            CompareResult::Same => {
+                println!("you won!");
+                return;
+            }
+            CompareResult::Diff { hit, blow } => {
+                println!("-> hit: {}, blow: {}", hit, blow);
+            }
+        }
+    }
 }
