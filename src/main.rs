822d08bb9ec5342f57f55a099791ab769b7fa72f
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-09-27 20:56:30 +0900

    履歴を表示

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use crate::lib::{CompareResult, Digits};
 /** ユーザー入力 */
 fn input() -> Digits {
     loop {
-        println!("input digits");
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
         let str = input.trim();
@@ -19,18 +18,29 @@ fn input() -> Digits {
 
 fn main() {
     let ans = Digits::generate_random();
+    let mut results: Vec<String> = vec![];
 
     for turn in 1..10 {
-        println!("turn: {}", turn);
-        let my_digits: Digits = input();
+        print!("\x1B[2J\x1B[1;1H"); // 画面クリア。カーソルを1行目1文字目にセット
+        for s in &results {
+            println!("{}", s);
+        }
 
+        println!("turn {}", turn);
+        println!("please input digits");
+
+        let my_digits: Digits = input();
         match my_digits.compare(&ans) {
             CompareResult::Same => {
                 println!("you won!");
                 return;
             }
             CompareResult::Diff { hit, blow } => {
-                println!("-> hit: {}, blow: {}", hit, blow);
+                let s = format!(
+                    "turn {}: \x1b[1;32m{}\x1b[0m -> hit: {}, blow: {}",
+                    turn, my_digits, hit, blow
+                );
+                results.push(s);
             }
         }
     }
